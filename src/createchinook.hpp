a74419d15686c02481d9f16c79c604fbc2a4532f
brendanglancy <bglance68@gmail.com>
2024-04-16 14:40:11 -0400

    update this before class

--- src/createchinook.hpp
+++ src/createchinook.hpp
@@ -9,8 +9,23 @@ class ChinookDB {
   ChinookDB();
   ~ChinookDB();
 
+  /**
+   * Checks to see if a state code is valid
+   * @param state_code
+   * @returns bool
+   */
   bool is_valid_state(const std::string& state_code);
+
+  /**
+   * Checks to see if the user is an admin, accorrding to the admin table
+   * @param user_id
+   * @returns bool
+   */
   bool is_user_admin(const std::string& user_id);
+
+  /**
+   *
+   */
   void view_tables();
 
  private:
--- src/pdr.hpp
+++ src/pdr.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <ctime>
 #include <fstream>
 #include <functional>
 #include <iostream>
@@ -9,7 +10,7 @@
 
 #include "../lib/Exceptions.hpp"
 #include "../lib/header.h"
-#include "createchinook.hpp" // Include the path to the ChinookDB header
+#include "createchinook.hpp"  // Include the path to the ChinookDB header
 
 struct PatientRecord {
   std::string ssn, firstName, middleInitial, lastName, address, city, stateCode,
@@ -18,9 +19,9 @@ struct PatientRecord {
 };
 
 class PDR {
-public:
+ public:
   PDR(ChinookDB &db)
-      : db(db) {} // Add a constructor to accept a ChinookDB reference
+      : db(db) {}  // Add a constructor to accept a ChinookDB reference
   using Callback = std::function<void()>;
   void collect_data();
   void save_data();
@@ -32,23 +33,23 @@ public:
    */
   void set_menu_callback(Callback callback);
 
-private:
-  PatientRecord patient;               // Holds the current patient's data
-  std::function<void()> Menu_Callback; // Callback for returning to the menu
-  ChinookDB &db; // Reference to the database for state validation
+ private:
+  PatientRecord patient;                // Holds the current patient's data
+  std::function<void()> Menu_Callback;  // Callback for returning to the menu
+  ChinookDB &db;  // Reference to the database for state validation
 
   // Input and validation methods
   bool valid_ssn(const std::string &ssn);
   bool valid_name(const std::string &name);
   bool valid_initial(const std::string &name);
   bool valid_state(
-      const std::string &state); // This will now interact with the database
+      const std::string &state);  // This will now interact with the database
   bool valid_zip(const std::string &zip);
 
   // Utility methods for getting and validating user input
-  std::string
-  get_valid_input(const std::string &prompt,
-                  const std::function<bool(const std::string &)> &validator);
+  std::string get_valid_input(
+      const std::string &prompt,
+      const std::function<bool(const std::string &)> &validator);
   std::string input_prompt(const std::string &prompt);
 
   std::string set_fname();
@@ -56,7 +57,7 @@ private:
   std::string set_lname();
   std::string set_address();
   std::string set_city();
-  std::string set_state(); // State input that will validate using the database
+  std::string set_state();  // State input that will validate using the database
   std::string set_zip();
   std::string set_ssn();
 
