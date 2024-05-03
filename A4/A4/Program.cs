5f5e0ce7646022ec15f0516435e8a60212ab819e
root <root@CORSAIRONE>
2024-05-02 21:06:03 -0400

    Im not logged in

--- A4/A4/Program.cs
+++ A4/A4/Program.cs
@@ -8,75 +8,27 @@ namespace HelloWorldSample
     {
         static void Main()
         {
-            using (var connection = new SqliteConnection("Data Source=hello.db"))
+            using (var connection = new SqliteConnection("Data Source=pdr.db"))
             {
                 connection.Open();
 
-                var command = connection.CreateCommand();
-                command.CommandText =
-                @"
-                    CREATE TABLE user (
-                        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                        name TEXT NOT NULL
-                    );
-
-                    INSERT INTO user
-                    VALUES (1, 'Brice'),
-                           (2, 'Alexander'),
-                           (3, 'Nate');
-                ";
-                command.ExecuteNonQuery();
-
-                Console.Write("Name: ");
-                var name = Console.ReadLine();
+                // Ask the User for A state code
+                Console.Write("State Code: ");
+                var code = Console.ReadLine();
 
                 #region snippet_Parameter
                 command.CommandText =
                 @"
-                    INSERT INTO user (name)
-                    VALUES ($name)
+                  Select NAME FROM states WHERE code = $code
                 ";
-                command.Parameters.AddWithValue("$name", name);
+                command.Parameters.AddWithValue("$code", code);
                 #endregion
                 command.ExecuteNonQuery();
 
-                command.CommandText =
-                @"
-                    SELECT last_insert_rowid()
-                ";
-                var newId = (long)command.ExecuteScalar();
-
-                Console.WriteLine($"Your new user ID is {newId}.");
+                var state = command.ExecuteScalar();
+                Console.WriteLine($"Your State is {state}.");
             }
 
-            Console.Write("User ID: ");
-            var id = int.Parse(Console.ReadLine());
-
-            #region snippet_HelloWorld
-            using (var connection = new SqliteConnection("Data Source=hello.db"))
-            {
-                connection.Open();
-
-                var command = connection.CreateCommand();
-                command.CommandText =
-                @"
-                    SELECT name
-                    FROM user
-                    WHERE id = $id
-                ";
-                command.Parameters.AddWithValue("$id", id);
-
-                using (var reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        var name = reader.GetString(0);
-
-                        Console.WriteLine($"Hello, {name}!");
-                    }
-                }
-            }
-            #endregion
         }
     }
 }
